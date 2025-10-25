34f2ace27d53788676c046e1e975de864aa71776
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-10-25 12:25:31 -0300

    Finalizando desafio

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,9 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite(conn));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
